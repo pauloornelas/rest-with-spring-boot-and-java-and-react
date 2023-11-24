e28223c91d100a89e8bb87e7e2a8e2410625e82e
Paulo Sérgio Ornelas Cardoso <paulo.ornelas.cardoso@gmail.com>
2023-11-24 16:09:41 -0300

    Ajuste URL para serviços

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://ec2-13-58-187-60.us-east-2.compute.amazonaws.com',
+    baseURL: 'http://localhost',
 });
 
 export default api;
